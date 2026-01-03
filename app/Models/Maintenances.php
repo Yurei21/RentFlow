962427136e1dc212d8884b69de997d843864bc56
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-01-03 19:03:09 +0800

    migrations incoming

---
+++ app/Models/Maintenances.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Maintenances extends Model
+{
+    /** @use HasFactory<\Database\Factories\MaintenancesFactory> */
+    use HasFactory;
+}
