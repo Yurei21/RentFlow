e7db7535190cf9a9f4065c5b80281056a752837a
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-04-21 22:08:32 +0800

    Policy adding

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
+use Illuminate\Support\Facades\Auth;
 
 class InvoiceController extends Controller
 {
@@ -13,7 +14,7 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
@@ -62,5 +63,31 @@ class InvoiceController extends Controller
     public function destroy(Invoice $invoice)
     {
         //
+    }
+    private function authorizeInvoiceOwner(Invoice $invoice, $asOwner = false)
+    {
+        $user = Auth::user();
+
+        if (!$invoice->group_id) {
+            if ($asOwner && $invoice->created_by !== $user->id) {
+                abort(403, 'Only the owner can perform this action.');
+            }
+
+            if (!$asOwner && $invoice->created_by !== $user->id) {
+                abort(403, 'You do not have access to this solo invoice.');
+            }
+        }
+
+        if ($invoice->group_id) {
+            $isMember = $invoice->group->users()->where('user_id', $user->id)->exists();
+
+            if (!$isMember) {
+                abort(403, 'you are not a member of this invoice');
+            }
+
+            if ($asOwner && $invoice->group->created_by !== $user->id && $invoice->created_by !== $user->id) {
+                abort(403, 'Only the invoice creatorr can perform this action.');
+            }
+        }
     }
 }
