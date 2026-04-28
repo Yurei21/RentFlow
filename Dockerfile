43e8e0c4c5e0f7a7582ebe45e4be9cdbf4e3c193
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-04-28 21:39:02 +0800

    Invoices incoming.

--- Dockerfile
+++ Dockerfile
@@ -46,10 +46,6 @@ RUN composer install \
 # Copy frontend build into Laravel public folder
 COPY --from=frontend-builder /app/public/build ./public/build
 
-# IMPORTANT: DO NOT run artisan cache here (causes build failures)
-# config cache should be done at runtime
-
-
 # ---------- Stage 3: Runtime ----------
 FROM php:8.4-cli AS runner
 WORKDIR /var/www/html
--- resources/js/Pages/Invoices/Index.jsx
+++ resources/js/Pages/Invoices/Index.jsx
@@ -1,22 +1,39 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link, router } from "@inertiajs/react";
 
-export default function Index() {
+export default function Index({invoices, queryParams = null, success}) {
+    queryParams = queryParams || {};
+    const [showSuccess, setShowSuccess] = useState(!!success);
+    const [search, setSearch] = useState(queryParams.search || "");
+    const [sortField, setSortField] = useState(
+        queryParams.sort_field || "created_at"
+    );
+    const [sortDirection, setSortDirection] = useState(
+        queryParams.sort_direction || "desc"
+    );
+
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [tenantToDelete, setTenantToDelete] = useState(null);
     return (
         <AuthenticatedLayout
             header={
                 <div className="flex justify-between items-center">
                     <h2 className="text-3xl font-semibold leading-tight text-gray-800 dark:text-gray-200 ">
-                        Projects
+                        Invoices
                     </h2>
                     <Link
-                        href={route("room.create")}
+                        href={route("invoice.create")}
                         className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
                     >
                         Add New
                     </Link>
                 </div>
             }
-        ></AuthenticatedLayout>
+        >
+            <Head title="Invoices" />
+            <div>
+
+            </div>
+        </AuthenticatedLayout>
     );
 }
