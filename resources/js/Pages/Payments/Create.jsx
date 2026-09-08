cce020686cdc9e91531a4dafbf2d8ddd55aecce2
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-09-08 22:22:39 +0800

    let's get back up

--- resources/js/Pages/Payments/Create.jsx
+++ resources/js/Pages/Payments/Create.jsx
@@ -1,10 +1,25 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link, router, useForm } from "@inertiajs/react";
 
-export default function Create() {
+export default function Create({tenants, invoices, groups}) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        
+        tenant_id: "",
+        invoice_id: "",
+        group_id: "",
+        amount_paid: "",
+        payment_date: "",
+        payment_method: "",
     });
+
+    const groupOptions = groups.map((group) => ({
+        value: group.id,
+        label: group.group_name
+    }));
+
+    const tenantsOption = tenants.map((tenant) => ({
+        value: tenant.id,
+        label: tenant.tenant_name
+    }));
     return (
         <AuthenticatedLayout
             header={
